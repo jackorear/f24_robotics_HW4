8e779a7f44c4116ad82d8a5cc7e480d14084c116
Jack <jorear@crimson.ua.edu>
2024-12-02 14:34:18 -0600

    trial ros bag

---
+++ turtlebot3_bringup/build/turtlebot3_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jack/f24_robotics_HW4/turtlebot3_bringup
+# Build directory: /home/jack/f24_robotics_HW4/turtlebot3_bringup/build/turtlebot3_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
